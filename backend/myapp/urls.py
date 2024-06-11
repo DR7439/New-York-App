0d0581e113f06e96e8c2aef4a1c49ba11beff655
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-06-11 14:44:28 +0100

    add search url

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -4,10 +4,11 @@ URL Configuration for the 'myapp' application.
 
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserCreate, MyTokenObtainPairView
+from .views import UserCreate, MyTokenObtainPairView, SearchCreate
 
 urlpatterns = [
     path('register/', UserCreate.as_view(), name='register'),
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/search/', SearchCreate.as_view(), name='search_create'),
 ]
