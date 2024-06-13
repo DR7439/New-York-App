7bf6143983e09f4accaf0260eb44e56da607b31d
hieulee7 <hieulee7798@gmail.com>
2024-06-13 23:57:19 +0700

    add font

--- frontend/myfrontend/src/App.js
+++ frontend/myfrontend/src/App.js
@@ -1,34 +1,44 @@
 // src/App.js
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-import { NavigationProvider } from './NavigationContext';
-import Login from './Login';
-import Register from './Register';
-import Dashboard from './Dashboard';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import { NavigationProvider } from "./NavigationContext";
+import Login from "./Login";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
+import { ConfigProvider } from "antd";
 
 const App = () => {
-    return (
-        <Router>
-            <NavigationProvider>
-                <AuthProvider>
-                    <Routes>
-                        <Route element={<PublicRoute />}>
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                        </Route>
-                        <Route element={<PrivateRoute />}>
-                            <Route path="/dashboard" element={<Dashboard />} />
-                        </Route>
-                        <Route path="*" element={<Navigate to="/dashboard" />} />
-                    </Routes>
-                </AuthProvider>
-            </NavigationProvider>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavigationProvider>
+        <ConfigProvider theme={{ hashed: false, token: {
+            fontFamily: 'Roboto',
+        } }}>
+          <AuthProvider>
+            <Routes>
+              <Route element={<PublicRoute />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Route>
+              <Route element={<PrivateRoute />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/dashboard" />} />
+            </Routes>
+          </AuthProvider>
+        </ConfigProvider>
+      </NavigationProvider>
+    </Router>
+  );
 };
 
 export default App;
