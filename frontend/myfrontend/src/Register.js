e1802b8df6eb4c6f50bdcd70f7b1589596138b5d
hieulee7 <hieulee7798@gmail.com>
2024-06-19 12:38:45 +0700

    update register page

--- frontend/myfrontend/src/Register.js
+++ frontend/myfrontend/src/Register.js
@@ -8,11 +8,11 @@ import LeftSide from "./components/authentication/LeftSide";
 const Register = () => {
   const [form] = Form.useForm();
   const { registerUser } = useContext(AuthContext);
-  const [username, setUsername] = useState("hieulee");
-  const [email, setEmail] = useState("hieulee@gmail.com");
-  const [password, setPassword] = useState("123456");
-  const [name, setName] = useState("hieu");
-  const [credits, setCredits] = useState(0);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name] = useState("");
+  const [credits] = useState(0);
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
@@ -36,6 +36,7 @@ const Register = () => {
       <Col span={12} className="flex items-center justify-center">
         <div className="space-y-4 mx-auto max-w-xl w-full">
           <h1 className="text-4xl font-medium">Welcome!</h1>
+          {error && <p className="text-red-500">{error}</p>}
           <Form form={form} onFinish={handleSubmit} layout="vertical">
             <Form.Item
               label="Username"
