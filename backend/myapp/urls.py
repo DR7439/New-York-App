e9129e7250a54bf8d7812bf2b8b644d543821fa5
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-06-19 15:56:42 +0100

    swagger

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -2,6 +2,7 @@
 URL Configuration for the 'myapp' application.
 """
 
+
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserCreate, MyTokenObtainPairView, SearchCreate
--- backend/target_advert/settings.py
+++ backend/target_advert/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'corsheaders',
-    "myapp", 
+    "myapp",
+    'drf_spectacular',
 ]
 
 REST_FRAMEWORK = {
@@ -54,6 +55,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'API documentation for your project',
+    'VERSION': '1.0.0',
+    # other settings
 }
 
 from datetime import timedelta
