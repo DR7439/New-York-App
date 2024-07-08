acad4b6d4c7596491a185d57882595e5dac36867
DR7439 <146729680+DR7439@users.noreply.github.com>
2024-07-08 14:14:09 +0100

    Updated paths

--- backend/test_predictions.py
+++ backend/test_predictions.py
@@ -5,13 +5,13 @@ from predictions_function import make_predictions
 
 def test_make_predictions():
     # Define a prediction time for testing
-    prediction_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    prediction_time = datetime.now().strftime('2024-05-02 00:00:00')
 
     path_to_zones_csv = './model_data/zones_df.csv'
     path_to_latest_historical_data_csv = './model_data/latest_historical_data.csv'
     path_to_pickle_file = './model_data/xgboost_busyness_model.pkl'
 
-    predictions = make_predictions(prediction_time, path_to_zones_csv, path_to_latest_historical_data_csv, path_to_pickle_file)
+    predictions = make_predictions(prediction_time,path_to_pickle_file, path_to_zones_csv, path_to_latest_historical_data_csv)
 
     # Print the predictions DataFrame
     print(predictions)
