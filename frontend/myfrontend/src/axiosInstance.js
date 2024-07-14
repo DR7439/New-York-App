1b4d627c4269dbfe6ddd56dd742cc7e282b89680
DR7439 <146729680+DR7439@users.noreply.github.com>
2024-07-14 23:26:46 +0100

    Edited axios

--- frontend/myfrontend/src/axiosInstance.js
+++ frontend/myfrontend/src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',  
-    withCredentials: true,  
+    baseURL: 'http://3.249.137.230:8000',
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
