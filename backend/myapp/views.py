3dbe12874f7b14d94c8b6aa060abe27e6608c888
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-07-23 12:25:15 +0100

    clean prints

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -212,8 +212,6 @@ class SearchAPIView(APIView):
                 CreditUsage.objects.create(user=user, credits_used=required_credits)
             
             user.save()
-
-            print("search_id: ", search_id)
             background_task.delay(search_id)
 
             # Invalidate cache for the user
@@ -883,7 +881,6 @@ class StripeWebhookView(View):
         payload = request.body.decode('utf-8')
         sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
         endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
-        print('endpoint: ', endpoint_secret)
         
 
         try:
@@ -891,30 +888,24 @@ class StripeWebhookView(View):
                 payload, sig_header, endpoint_secret
             )
         except ValueError as e:
-            print(f"ValueError: {e}")
             return HttpResponse(status=400)
         except stripe.error.SignatureVerificationError as e:
-            print(f"SignatureVerificationError: {e}")
             return HttpResponse(status=400)
 
-        print(f"Event type: {event['type']}")
+        
 
         if event['type'] == 'payment_intent.succeeded':
             payment_intent = event['data']['object']
             user_id = payment_intent['metadata'].get('user_id')
             amount_received = payment_intent['amount_received']  # amount_received is in cents
 
-            print(f"Payment Intent: {payment_intent}")
-            print(f"User ID: {user_id}")
-            print(f"Amount Received: {amount_received}")
+           
 
             try:
                 user = CustomUser.objects.get(id=user_id)
                 user.credits += int(amount_received) // 10  
                 user.save()
-                print(f"User {user.username} credits updated to {user.credits}")
             except CustomUser.DoesNotExist:
-                print(f"User with ID {user_id} does not exist")
                 return HttpResponse(status=404)
 
         return HttpResponse(status=200)
