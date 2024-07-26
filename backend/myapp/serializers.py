8b40f1b01f261878d6db3ff8029fbf639a32e487
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-07-26 15:57:44 +0100

    clean serializers

--- backend/myapp/serializers.py
+++ backend/myapp/serializers.py
@@ -1,40 +1,6 @@
-"""
-Serializers for the 'myapp' application.
-
-This module defines the serializers used for serializing and deserializing user data and handling 
-JWT token generation with custom claims.
-
-Classes:
-    UserSerializer: Serializer for the CustomUser model, handling user creation and serialization.
-    MyTokenObtainPairSerializer: Serializer for customizing JWT token claims.
-    SearchSerializer: Serializer for the Search model, handling the conversion between 
-                      Search instances and JSON representations.
-
-"""
-
 from rest_framework import serializers
-from .models import  Busyness, Demographic
 
 
-class BusynessSerializer(serializers.ModelSerializer):
-    time = serializers.DateTimeField(source='datetime')
-    score = serializers.FloatField(source='busyness_score')
-
-    class Meta:
-        model = Busyness
-        fields = ['time', 'score']
-
-class DemographicSerializer(serializers.ModelSerializer):
-    demographic_score = serializers.FloatField(source='score')
-
-    class Meta:
-        model = Demographic
-        fields = ['demographic_score']
-
-class ZoneScoresSerializer(serializers.Serializer):
-    zone_id = serializers.IntegerField(source='zone.id')
-    demographic_score = serializers.FloatField()
-    busyness_scores = BusynessSerializer(many=True)
 
  
 class PredictionRequestSerializer(serializers.Serializer):
