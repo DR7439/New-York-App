b7041cf5fe4f83bffb88e7b396e63ef5ee2e4127
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-07-26 16:31:19 +0100

    fix urls spelling

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -8,10 +8,10 @@ from .views import TopZonesView,  ZoneScoresByDatetimeView, ZoneDetailsBySearchD
 urlpatterns = [
 
 
-    path('analytics/top-zones/', TopZonesView.as_view(), name='top-zones'), 
-    path('analytics/zone-scores-by-datetime/', ZoneScoresByDatetimeView.as_view(), name='zone-scores-by-datetime'), #think used for map
-    path('analytics/zone-details-by-search-date-zone/', ZoneDetailsBySearchDateZoneView.as_view(), name='zone-details-by-search-date-zone'),   #used for line chart
-    path('analytics/recommend-advertising-locations/', RecommendAdvertisingLocationsView.as_view(), name='recommend-advertising-locations'),
+    path('top-zones/', TopZonesView.as_view(), name='top-zones'), 
+    path('zone-scores-by-datetime/', ZoneScoresByDatetimeView.as_view(), name='zone-scores-by-datetime'), #think used for map
+    path('zone-details-by-search-date-zone/', ZoneDetailsBySearchDateZoneView.as_view(), name='zone-details-by-search-date-zone'),   #used for line chart
+    path('recommend-advertising-locations/', RecommendAdvertisingLocationsView.as_view(), name='recommend-advertising-locations'),
     
 
 ]
