7502bcd1bb09c5fa9337df9e0ee25d07b0df35c1
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-07-26 16:36:34 +0100

    fix miss spelled url

--- backend/zones/urls.py
+++ backend/zones/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', ZoneListView.as_view(), name='zone-list'),
     path('<int:zone_id>/interests', InterestZoneCountByZoneView.as_view(), name='interest-zone-counts-by-zone'),
     path('<int:zone_id>/details/', ZoneDetailView.as_view(), name='zone-details'),
-    path('<int:zone_id>/billboards', BillboardsByZoneView.as_view(), name='billboards-by-zone'),
+    path('<int:zone_id>/billboards/', BillboardsByZoneView.as_view(), name='billboards-by-zone'),
     path('interests/', InterestAPIView.as_view(), name='interests'),
 
     
